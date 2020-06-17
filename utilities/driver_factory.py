8121236d6c063744a75e9dc399fbb51716548402
Prasanth <prasanth.chebrolu@gmail.com>
2020-06-17 11:09:15 -0600

    Updated driver_factory

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,26 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 import config
-caps = DesiredCapabilities.FIREFOX
-caps["Marionette"] = True
-caps["binary"] = "Applications/Firefox.app/Contents/MacOS/firefox-bin"
-#driver = webdriver.Chrome("/Users/prasanth/workspace/MENTEE-Prasanth/drivers/chromedriver")
-#driver = webdriver.Firefox(capabilities=caps,executable_path="/Users/prasanth/workspace/MENTEE-Prasanth/drivers/geckodriver")
-#driver.get("http://automationpractice.com/index.php")
-#driver.maximize_window()
-#title = driver.title
-#print(title)
-#assert "My Store" in title
+
 
 
 class Driver:
 
     def get_driver(self, browser_name):
         if browser_name == "chrome":
-            driver = webdriver.Chrome("/Users/prasanth/workspace/MENTEE-Prasanth/drivers/chromedriver")
+            driver = webdriver.Chrome(ChromeDriverManager().install())
         elif browser_name == "firefox":
-            driver = webdriver.Firefox(capabilities=caps,
-                                   executable_path="/Users/prasanth/workspace/MENTEE-Prasanth/drivers/geckodriver")
+            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         return driver
 
     def launch_url(self, url, driver):
