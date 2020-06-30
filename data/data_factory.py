29bfe9d9ffc0423a0a9f8be5eeb611cf0f683d4e
Prasanth <prasanth.chebrolu@gmail.com>
2020-06-29 22:55:47 -0600

    Added methods to read and write json files

--- data/data_factory.py
+++ data/data_factory.py
@@ -11,3 +11,14 @@ text = jsonObject['text']
 print(product)
 print(overall)
 print(text)
+
+
+json_data = {
+    "product":"Python book",
+    "overall":"4.0",
+    "text":"Nice book"
+}
+
+with open('writed_json.json', 'w') as jsonFile:
+    json.dump(json_data, jsonFile)
+    jsonFile.close()
